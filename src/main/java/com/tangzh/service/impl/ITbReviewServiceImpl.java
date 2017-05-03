543360ff815ee27b3a36656a896356b71b829422
tangzh <tangzh@huaweisoft.com>
2017-05-03 16:49:11 +0800

    all impl

--- src/main/java/com/tangzh/service/impl/ITbReviewServiceImpl.java
+++ src/main/java/com/tangzh/service/impl/ITbReviewServiceImpl.java
@@ -15,72 +15,72 @@ import com.tangzh.service.ITbReviewService;
 public class ITbReviewServiceImpl implements ITbReviewService{
 
 	@Resource
-	ReviewMapper repairMapper;
+	ReviewMapper repviewMapper;
 	
 	@Override
 	public int countByExample(ReviewExample example) {
 		
-		return repairMapper.countByExample(example);
+		return repviewMapper.countByExample(example);
 	}
 
 	@Override
 	public int deleteByExample(ReviewExample example) {
 		
-		return repairMapper.deleteByExample(example);
+		return repviewMapper.deleteByExample(example);
 	}
 
 	@Override
 	public int deleteByPrimaryKey(Integer id) {
 		
-		return repairMapper.deleteByPrimaryKey(id);
+		return repviewMapper.deleteByPrimaryKey(id);
 	}
 
 	@Override
 	public int insert(Review record) {
 		
-		return repairMapper.insert(record);
+		return repviewMapper.insert(record);
 	}
 
 	@Override
 	public int insertSelective(Review record) {
 		
-		return repairMapper.insertSelective(record);
+		return repviewMapper.insertSelective(record);
 	}
 
 	@Override
 	public List<Review> selectByExample(ReviewExample example) {
 		
-		return repairMapper.selectByExample(example);
+		return repviewMapper.selectByExample(example);
 	}
 
 	@Override
 	public Review selectByPrimaryKey(Integer id) {
 		
-		return repairMapper.selectByPrimaryKey(id);
+		return repviewMapper.selectByPrimaryKey(id);
 	}
 
 	@Override
 	public int updateByExampleSelective(Review record, ReviewExample example) {
 		
-		return repairMapper.updateByExampleSelective(record, example);
+		return repviewMapper.updateByExampleSelective(record, example);
 	}
 
 	@Override
 	public int updateByExample(Review record, ReviewExample example) {
 		
-		return repairMapper.updateByExample(record, example);
+		return repviewMapper.updateByExample(record, example);
 	}
 
 	@Override
 	public int updateByPrimaryKeySelective(Review record) {
 		
-		return repairMapper.updateByPrimaryKeySelective(record);
+		return repviewMapper.updateByPrimaryKeySelective(record);
 	}
 
 	@Override
 	public int updateByPrimaryKey(Review record) {
 		
-		return repairMapper.updateByPrimaryKey(record);
+		return repviewMapper.updateByPrimaryKey(record);
 	}
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,6 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-
-<%
-response.sendRedirect("/pages/Login.jsp");
-%>
+<html>
+<head>
+</head>
+<body>
+Hello!!!
+</body>
+</html>
