892cd0fdd32ff8cb144d7dcf5dc1fc2648551d8c
tangzh <tangzh@huaweisoft.com>
2017-05-04 14:40:08 +0800

    Add first controller ,test browser

---
+++ src/main/java/com/tangzh/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.tangzh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping()
+public class PageController {
+	@RequestMapping("/login.do")
+	public String login(){
+		return "/login";
+	}
+
+}
