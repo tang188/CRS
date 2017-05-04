efe38ced650926826309344810c4d7043a6763ee
tangzh <tangzh@huaweisoft.com>
2017-05-04 17:41:45 +0800

    jsp Error

--- src/main/java/com/tangzh/controller/PageController.java
+++ src/main/java/com/tangzh/controller/PageController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping()
+@RequestMapping("page")
 public class PageController {
+	
 	@RequestMapping("/login.do")
 	public String login(){
-		return "/login";
+		return "login";
 	}
-
+	
+	@RequestMapping("/register.do")
+	public String register(){
+		return "register";		
+	}
 }
